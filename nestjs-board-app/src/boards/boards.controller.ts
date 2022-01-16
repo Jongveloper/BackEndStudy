9a7114687b356bb0237126ffd0f2a2259cb44d01
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-16 21:42:08 +0900

    ✨특정 ID로 게시물 가져오기

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,5 +1,5 @@
 import { BoardsService } from './boards.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -15,5 +15,10 @@ export class BoardsController {
   @Post()
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
   }
 }
