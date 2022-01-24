5e4940e5560f71b60be261990c96fde075a1eeae
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-24 22:54:52 +0900

    :memo: CatDataMocking

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send({ cats: app_model_1.Cat });
 });
 app.listen(port, function () {
-    console.log("Example app listening on port http://localhost:" + port);
+    console.log("server is on!");
 });
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
+import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
 const port: number = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send({ cats: Cat });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`server is on!`);
 });
