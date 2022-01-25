a3f2c7e5bc46bd66f35aefbe30b5de5f7e5e2bf1
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-25 22:16:46 +0900

    :memo: middleware

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -4,8 +4,27 @@ import { Cat, CatType } from './app.model';
 const app: express.Express = express();
 const port: number = 8000;
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('this is logging middleware');
+  next();
+});
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
+});
+
+app.get('/cats/blue', (req: express.Request, res: express.Response) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get('/cats/som', (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log('this is error middleware');
+  res.send({ error: '404 not found error' });
 });
 
 app.listen(port, () => {
