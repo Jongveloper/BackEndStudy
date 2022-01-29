055c24642f794b65aedc47d74e41516b3f1018c4
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-30 00:39:22 +0900

    :wastebasket:

--- nestjs-a-to-z/project/src/cats/cats.service.ts
+++ nestjs-a-to-z/project/src/cats/cats.service.ts
@@ -3,7 +3,6 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
-import { Cat } from './cats.schema';
 import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
