02ffd8791288e6ff4743993022da2e971a5b023b
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-02-06 01:22:13 +0900

    :tada: chattings

---
+++ chattings/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
