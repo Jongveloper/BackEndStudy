9d379ff0ea3d9e75c35ca1338abb9d0bfcc14ed6
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-02-06 14:34:46 +0900

    :memo: gateway lifecycle

---
+++ chattings/src/chats/chats.gateway.ts
@@ -0,0 +1,39 @@
+import { Socket } from 'socket.io';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+  @SubscribeMessage('new_user')
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log(socket.id);
+    console.log(username);
+    socket.emit('hello_user', 'hello ' + username);
+  }
+}
