cf382560619db8e374f0ce4e19b4131bfe90d61b
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-02-10 22:46:47 +0900

    :card_file_box: chats Model

--- chattings/src/chats/chats.module.ts
+++ chattings/src/chats/chats.module.ts
@@ -1,7 +1,16 @@
+import { SocketSchema, Socket as SocketModel } from './models/socket.model';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
+import { Chatting, ChattingSchema } from './models/chattings.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Chatting.name, schema: ChattingSchema },
+      { name: SocketModel.name, schema: SocketSchema },
+    ]),
+  ],
   providers: [ChatsGateway],
 })
 export class ChatsModule {}
